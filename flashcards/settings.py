f2e2afc2ffbe1e13076982b6f972521e0cd4f9ff
Chet Weger <chetweger@gmail.com>
2013-03-24 12:36:47 -0700

    Modified settings.

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -7,12 +7,21 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'chetweger@gmail.com'),
+     #('Chet Weger', 'chetweger@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config()}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'flashcards',                      # Or path to database file if using sqlite3.
+        'USER': 'chet',                      # Not used with sqlite3.
+        'PASSWORD': '1532forfreedom',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
